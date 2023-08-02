74f894ff0ac8a1ba75eb60b1afadedf613f27345
이상협 <75459370+723poil@users.noreply.github.com>
2023-08-02 23:30:29 +0900

    [Silver V] Title: 행렬 곱셈, Time: 84 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -4,22 +4,27 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-A = list()
-
-for i in range(N):
-    A.append(list(map(int, input().split())))
+A = [list(map(int, input().split())) for _ in range(N)]
     
 M, K = map(int, input().split())
 
-B = list()
+B = list(zip( *[list(map(int, input().split())) for _ in range(M)]))
 
-for i in range(M):
-    B.append(list(map(int, input().split())))
+# for i in range(M):
+#     B.append(list(map(int, input().split())))
+
+def get_sum(a:list, b:list):
+    sum = 0
+    for i in range(len(a)):
+        sum += a[i] * b[i]
+    
+    return sum
+
+result = [[0 for _ in range(K)] for _ in range(N)]
     
 for i in range(N):
     for j in range(K):
-        sum = 0
-        for k in range(M):
-            sum += (A[i][k] * B[k][j])
-        print(sum, end=" ")
-    print()
+        result[i][j] = str(get_sum(A[i], B[j]))
+        
+for i in range(N):
+    print(" ".join(result[i]))
