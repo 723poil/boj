59d36b9670fed047525dab7e7bf139061d1d8bd4
이상협 <75459370+723poil@users.noreply.github.com>
2024-03-25 20:18:24 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2660 ms, Memory: 86264 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,51 +1,57 @@
-import sys
+from sys import stdin
 from collections import deque
 
-input = sys.stdin.readline
+input = stdin.readline
 
-N, M = map(int, input().split())
-
-maap = [[] for _ in range(N+1)]
-
-for i in range(1,N+1):
-    maap[i] = (list(str(input())))
-    maap[i].insert(0, 0)
-
-def BFS():
-    dy = [0, 1, 0, -1]
-    dx = [1, 0, -1, 0]
-
-    visited = [[False for _ in range(M+1)] for _ in range(N+1)]
-    breakvisited = [[False for _ in range(M+1)] for _ in range(N+1)]
-    visited[1][1] = True
-    queue = deque()
-    queue.append([1, 1, 1, False])
-    while queue:
-        y, x, cost, isbreak = queue.popleft()
-
-        if y == N and x == M:
-            print(cost)
-            return
-
-        for i in range(4):
-            sy = y + dy[i]
-            sx = x + dx[i]
-
-            if 0<sy<=N and 0<sx<=M and visited[sy][sx] == False:
-                if isbreak:
-                    if int(maap[sy][sx]) == 0 and breakvisited[sy][sx] == False:
-                        breakvisited[sy][sx] = True
-                        queue.append([sy,sx,cost+1, isbreak])
-                else:
-                    if int(maap[sy][sx]) == 0:
-                        visited[sy][sx] = True
-                        queue.append([sy, sx, cost+1, isbreak])
-                    else:
-                        visited[sy][sx] = True
-                        queue.append([sy, sx, cost+1, True])
-
+def find_finish(maps: list, pos: tuple):
+    x = [0, 1, 0, -1]
+    y = [1, 0, -1, 0]
     
-    print(-1)
-    return
+    n, m = pos
+    
+    searched = [[False for _ in range(m)] for _ in range(n)]
+    searched_broken = [[False for _ in range(m)] for _ in range(n)]
+    
+    qq = deque()
+    
+    qq.append(((0,0), 1, False))
+    searched[0][0] = True
+    
+    while qq:
+        cp, cc, cb = qq.popleft()
+        
+        if cp[0] == n-1 and cp[1] == m-1:
+            return cc
+        
+        for i in range(4):
+            curx = cp[1] + x[i]
+            cury = cp[0] + y[i]
+            
+            able_pos = 0 <= curx < m and 0 <= cury < n
+            
+            if able_pos and not searched[cury][curx]:
+                if cb:
+                    if maps[cury][curx] == 0 and not searched_broken[cury][curx]:
+                        searched_broken[cury][curx] = True
+                        qq.append(((cury, curx), cc + 1, cb))
+                else:
+                    if maps[cury][curx] == 0:
+                        searched[cury][curx] = True
+                        qq.append(((cury, curx), cc + 1, cb))
+                    else:
+                        searched[cury][curx] = True
+                        qq.append(((cury, curx), cc + 1, True))
+        
+    return -1
 
-BFS()
+def solution():
+    N, M = map(int, input().split())
+    
+    maps = []
+    
+    for _ in range(N):
+        maps.append(list(map(int, list(str(input().strip())))))
+        
+    return find_finish(maps, (N, M))
+
+print(solution())
