798274733c16845fb95cfd6d3d7838ec2182ebae
이상협 <75459370+723poil@users.noreply.github.com>
2024-04-19 23:38:21 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 56 ms, Memory: 32524 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -4,50 +4,36 @@ setrecursionlimit(10 ** 5)
 
 input = stdin.readline
 
-class Node:
+
+def postorder(start, end):
+    if start >= end:
+        print(tree_list[start])
+        return
     
-    def __init__(self, data: int = None) -> None:
-        self.data = data
-        self.lc = None
-        self.rc = None
-        
-class Tree:
+    if tree_list[start] > tree_list[end] or tree_list[start] < tree_list[start + 1]:
+        postorder(start+1, end)
+        print(tree_list[start])
+        return
     
-    def __init__(self, data: int) -> None:
-        self.root = Node(data)
-        
-    def insert(self, data: int):
-        cur_node = self.root
-        
-        child = cur_node.lc if data < cur_node.data else cur_node.rc
-        
-        while child:
-            cur_node = child
-            child = cur_node.lc if data < cur_node.data else cur_node.rc
-            
-        if data < cur_node.data:
-            cur_node.lc = Node(data)
-        else:
-            cur_node.rc = Node(data)
-            
-    def print(self, node: Node):
-        if not node:
-            return
-        
-        self.print(node.lc)
-        self.print(node.rc)
-        print(node.data)
-            
+    mid = 0
+    
+    for i in range(start + 1, end + 1):
+        if tree_list[start] < tree_list[i]:
+            mid = i
+            break
+    
+    postorder(start+1, mid - 1)
+    postorder(mid, end)
+    print(tree_list[start])
+
 
 if __name__ == '__main__':
     
-    n = int(input())
-    tree = Tree(n)
+    tree_list = []
     
     n = input()
     while n:
-        tree.insert(int(n))
-        
+        tree_list.append(int(n))
         n = input()
-       
-    tree.print(tree.root)
+        
+    postorder(0, len(tree_list) - 1)
